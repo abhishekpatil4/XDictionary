a8e59fbcbd9d922d1cab9a98c81e8fa424904ffb
Abhishek Patil <abishkpatil@gmail.com>
2024-06-07 13:15:04 +0530

    final

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,49 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// import './App.css'
+
+const dict = [
+
+  { word: "React", meaning: "A JavaScript library for building user interfaces." },
+
+  { word: "Component", meaning: "A reusable building block in React." },
+
+  { word: "State", meaning: "An object that stores data for a component." }
+
+]
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  const [defn, setDefn] = useState("");
+  const [submitted, setSubmitted] = useState(false);
+  const handleSearch = (e) => {
+    setSubmitted(true);
+    e.preventDefault();
+    const data = new FormData(e.target);
+    const payload = Object.fromEntries(data);
+    const searchedWord = payload.word.trim();
+    for (let i = 0; i < dict.length; i++) {
+      if (dict[i].word.toLocaleLowerCase() == searchedWord.toLocaleLowerCase()) {
+        setDefn(dict[i].meaning);
+        break;
+      }
+    }
+  }
+  return <div>
+    <h1>Dictionary</h1>
+    <form onSubmit={handleSearch}>
+      <input required type="text" name="word" placeholder='Search for a word...' />
+      <button type="submit">Search</button>
+    </form>
+    <div>
+      <h3>Definition:</h3>
+      {
+        submitted &&
+        (
+          defn !== "" ? <p>{defn}</p> : <p>Word not found in the dictionary.</p>
+        )
+      }
+    </div>
+  </div>
 }
 
 export default App
